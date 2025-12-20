fb7175aff73c71eab20830b562de75b70a82ee09
Marko Korac <marko.rockyland@gmail.com>
2025-12-20 19:43:50 +0100

    next.config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,27 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+
+  async redirects() {
+    return [
+      // 1) root -> default locale
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true,
+      },
+
+      // 2) sve bez locale prefiksa -> /en/...
+      // (izuzimamo Next interne rute i "specijalne" fajlove)
+      {
+        source:
+          '/((?!en/|es/|sr/|_next/|api/|favicon.ico|robots.txt|sitemap.xml|sitemap-news.xml|opensearch.xml|ads.txt|app-ads.txt|manifest.webmanifest).*)',
+        destination: '/en/$1',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
