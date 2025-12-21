4159fae03dc081298ef6d0c743482fdc436bdd52
Marko Korac <marko.rockyland@gmail.com>
2025-12-21 11:01:38 +0100

    SEO: add news sitemap to robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,15 @@
 // src/app/robots.ts
-import type {MetadataRoute} from 'next';
+import type { MetadataRoute } from 'next';
 
 const SITE = (process.env.NEXT_PUBLIC_SITE_URL ?? 'https://tech.infohelm.org').replace(/\/$/, '');
 
 export default function robots(): MetadataRoute.Robots {
   return {
     host: SITE,
-    sitemap: `${SITE}/sitemap.xml`,
-    rules: [
-      { userAgent: '*', allow: '/' }
-    ]
+    sitemap: [
+      `${SITE}/sitemap.xml`,
+      `${SITE}/sitemap-news.xml`
+    ],
+    rules: [{ userAgent: '*', allow: '/' }]
   };
 }
